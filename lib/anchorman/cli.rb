b1b3eefaf67aa05e93764385e91075713f215b5e
Davis W. Frank <dwfrank+github@infe.ws>
2013-03-08 08:31:04 -0800

    Refactoring; adding color to output

--- lib/anchorman/cli.rb
+++ lib/anchorman/cli.rb
@@ -6,27 +6,31 @@ module Anchorman
     desc "generate", "Generates a draft release notes document"
 
     def generate
-      commits = begin
-        repo = Git.open('.')
-        log = repo.log
-        say "#{log.size} commit(s) found"
-        log
-      rescue ArgumentError
-        puts "No git repo found"
-      rescue Git::GitExecuteError
-        puts "No git log found"
-      end
+      commits = get_commits
 
       return unless commits.size
 
       empty_directory 'anchorman'
 
       create_file 'anchorman/release_notes.md' do
-
         "# Release Notes\n #{commits.first.sha}"
       end
 
     end
 
+    no_tasks do
+
+      def get_commits
+        repo = Git.open('.')
+        log = repo.log
+        say "#{log.size} commit(s) found", :green
+        log
+      rescue ArgumentError
+        say 'No git repo found', :red
+      rescue Git::GitExecuteError
+        say 'No git log found', :red
+      end
+
+    end
   end
 end
