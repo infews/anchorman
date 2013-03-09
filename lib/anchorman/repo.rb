c692f6617cda26bcff015641d07c1951de7d8b9a
Davis W. Frank <dwfrank+github@infe.ws>
2013-03-08 21:23:21 -0800

    A little extraction.
     Please enter the commit message for your changes. Lines starting

--- lib/anchorman/repo.rb
+++ lib/anchorman/repo.rb
@@ -1,17 +1,21 @@
 module Anchorman
   class Repo < Struct.new(:git_repo)
 
+    def remote
+      git_repo.remote
+    end
+
     def is_github?
-      git_repo.remote &&
-          git_repo.remote.name == "origin" &&
-          git_repo.remote.url &&
-          git_repo.remote.url.match(/github\.com/)
+      remote &&
+          remote.name == "origin" &&
+          remote.url &&
+          remote.url.match(/github\.com/)
     end
 
     def commit_url(sha)
       return sha unless is_github?
 
-      "#{git_repo.remote.url[0..-5]}/commits/#{sha}"
+      "#{remote.url[0..-5]}/commits/#{sha}"
     end
   end
 end
